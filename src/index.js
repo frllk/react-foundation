c9f0a776585be927ed5b3c7ee8c16906c5d20d59
frllk <1402805300@qq.com>
2020-07-13 15:56:54 +0800

    jsx基本使用及原理

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
+// react入口文件
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// 使用React.createElement创建react元素
+// const h1 = React.createElement('h1', { id: 'test' }, 'hello react！')
+// const z = React.createElement('span', { style: { display: 'block', color: 'red' } }, '我是span标签！')
+// const p = React.createElement('p', { style: { background: 'blue' } }, '我是p标签！', z)
+// const div1 = React.createElement('div', { className: 'div' }, h1, p, z, 1000, 'str')
+
+// 使用jsx创建react元素
+// jsx：标记语言  jsx：html标签都可以写，支持所有html标记
+const h1 = <div className="div">
+  <h1>hello jsx!</h1>
+  <p>我是p标签</p>
+  <span>我是span</span>
+  <div>
+    <h2>我是h2</h2>
+  </div>
+</div>
+
+// 对比
+let div1 = <div>我是div1</div>
+let div2 = React.createElement('div', null, '我是div2')
+console.log(div1, div2);
+
+// 只能有一个根标签，采用小驼峰命名
+const div3 = (
+  <div className='div'>
+    <div>
+      <h1>12345</h1>
+    </div>
+  </div>
+
+)
+
+ReactDOM.render(div3,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
